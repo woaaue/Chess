ae92bd6a03e282f8afdb74f7773efe1278d7eb01
woaaue <124384635+woaaue@users.noreply.github.com>
2024-02-09 11:38:53 +0300

    figure arrangement

--- Assets/Scripts/Model/ChessBoard/ChessBoard.cs
+++ Assets/Scripts/Model/ChessBoard/ChessBoard.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 public class ChessBoard
 {
     private int _boardSize;
@@ -7,19 +9,49 @@ public class ChessBoard
     {
         _boardSize = 8;
         InitializeBoard();
+        FillBoard();
     }
 
     private void InitializeBoard()
     {
         _cells = new Cell[_boardSize, _boardSize];
 
-        for (int x = 0; x < _boardSize; x++)
+        for (int y = 0; y < _boardSize; y++)
         {
-            for (int y = 0; y < _boardSize; y++)
+            for (int x = 0; x < _boardSize; x++)
             {
-                _cells[x, y] = new Cell(x, y);
+                _cells[y, x] = new Cell(_boardSize - y, x + 1);
             }
         }
+    }
+
+    private void FillBoard()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            PlaceFigure(GetCell(1, i), new Pawn(GetCell(1, i), Color.white));
+            PlaceFigure(GetCell(6, i), new Pawn(GetCell(6, i), Color.black));
+        }
+
+        PlaceFigure(GetCell(0, 0), new Rook(GetCell(0, 0), Color.white));
+        PlaceFigure(GetCell(0, 7), new Rook(GetCell(0, 7), Color.white));
+        PlaceFigure(GetCell(7, 0), new Rook(GetCell(7, 0), Color.black));
+        PlaceFigure(GetCell(7, 7), new Rook(GetCell(7, 7), Color.black));
+
+        PlaceFigure(GetCell(0, 1), new Knight(GetCell(0, 1), Color.white));
+        PlaceFigure(GetCell(0, 6), new Knight(GetCell(0, 6), Color.white));
+        PlaceFigure(GetCell(7, 1), new Knight(GetCell(7, 1), Color.black));
+        PlaceFigure(GetCell(7, 6), new Knight(GetCell(7, 6), Color.black));
+
+        PlaceFigure(GetCell(0, 2), new Bishop(GetCell(0, 2), Color.white));
+        PlaceFigure(GetCell(0, 5), new Bishop(GetCell(0, 5), Color.white));
+        PlaceFigure(GetCell(7, 2), new Bishop(GetCell(7, 2), Color.black));
+        PlaceFigure(GetCell(7, 5), new Bishop(GetCell(7, 5), Color.black));
+
+        PlaceFigure(GetCell(0, 3), new Queen(GetCell(0, 3), Color.white));
+        PlaceFigure(GetCell(0, 4), new King(GetCell(0, 4), Color.white));
+        PlaceFigure(GetCell(7, 3), new Queen(GetCell(7, 3), Color.black));
+        PlaceFigure(GetCell(7, 4), new King(GetCell(7, 4), Color.black));
     }
 
     public Cell GetCell(int x, int y)
