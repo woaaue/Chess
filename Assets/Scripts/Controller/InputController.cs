7691aa8a3dce4203a5967b271664a0a36fcff59a
woaaue <124384635+woaaue@users.noreply.github.com>
2024-02-10 17:00:31 +0300

    edit input controller

--- Assets/Scripts/Controller/InputController.cs
+++ Assets/Scripts/Controller/InputController.cs
@@ -2,20 +2,30 @@ using UnityEngine;
 
 public sealed class InputController : MonoBehaviour
 {
-    private Camera _camera;
     private Match _match;
+    private Camera _camera;
 
     private void Start()
     {
-        _camera = Camera.main;
         _match = new Match();
+        _camera = Camera.main;
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            _match.FirstClickCell(_camera.ScreenToWorldPoint(Input.mousePosition));
+            _match.IsCellOccupied(_match.CoordinateConversion(_camera.ScreenToWorldPoint(Input.mousePosition)));
+
+            if (_match._selectedFigure == null)
+            {
+                return;
+            }
+            else if (_match.IsPossibleMove(_match._selectedFigure, _match.CoordinateConversion(_camera.ScreenToWorldPoint(Input.mousePosition))))
+            {
+                _match.ManipulationFigure(_match._selectedFigure, _match.CoordinateConversion(_camera.ScreenToWorldPoint(Input.mousePosition)));
+                _match.SwitchPlayer();
+            }
         }
     }
 }
